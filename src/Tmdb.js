2828b4d20efa20c89d86a879b719fc0927d70ed4
EnriqueSFlor <enriquesilvaflor@gmail.com>
2022-05-02 22:05:15 -0300

    subindo lista

--- src/Tmdb.js
+++ src/Tmdb.js
@@ -1,48 +1,70 @@
 const API_KEY = "70c1c78ab7ae6f82f6cd31b404f57f9f";
 const API_BASE = "https://api.themoviedb.org/3";
 
+const basicFetch = async (endpoint) => {
+  const req = await fetch(`${API_BASE}${endpoint}`);
+  const json = await req.json();
+  return json;
+};
+
 export default {
   getHomeList: async () => {
     return [
       {
         slug: "originals",
         title: "Originais do Netflix",
-        items: [],
+        items: await basicFetch(
+          `/discover/tv?with_network=213&language=pt-BR&api_key${API_KEY}`
+        ),
       },
       {
         slug: "trending",
         title: "Recomendados para Você",
-        items: [],
+        items: await basicFetch(
+          `/trending/all/week?language=pt-BR&api_key${API_KEY}`
+        ),
       },
       {
         slug: "toprated",
         title: "Em alta",
-        items: [],
+        items: await basicFetch(
+          `/movie/top_rated?language=pt-BR&api_key${API_KEY}`
+        ),
       },
       {
         slug: "action",
         title: "Ação",
-        items: [],
+        items: await basicFetch(
+          `/discover/movie?with_genres=28&language=pt-BR&api_key=${API_KEY}`
+        ),
       },
       {
         slug: "comedy",
         title: "Comédia",
-        items: [],
+        items: await basicFetch(
+          `/discover/movie?with_genres=35&language=pt-BR&api_key=${API_KEY}`
+        ),
       },
       {
         slug: "horror",
         title: "Terror",
-        items: [],
+        items: await basicFetch(
+          `/discover/movie?with_genres=27&language=pt-BR&api_key=${API_KEY}`
+        ),
       },
       {
         slug: "romance",
         title: "Romance",
-        items: [],
+        items: await basicFetch(
+          `/discover/movie?with_genres=10749&language=pt-BR&api_key=${API_KEY}`
+        ),
       },
       {
         slug: "documentary",
         title: "Documentários",
-        items: [],
+        items: await basicFetch(
+          `/discover/movie?with_genres=99&language=pt-BR&api_key=${API_KEY}`
+        ),
       },
     ];
   },
