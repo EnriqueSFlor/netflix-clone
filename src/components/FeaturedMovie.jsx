cd97f3a582749b2cf41723aff0dc94303c94bed5
EnriqueSFlor <enriquesilvaflor@gmail.com>
2022-05-05 21:47:08 -0300

    subindo infos

--- src/components/FeaturedMovie.jsx
+++ src/components/FeaturedMovie.jsx
@@ -1,6 +1,12 @@
 import "./FeaturedMovie.css";
 
 export default ({ item }) => {
+  console.log(item.first_air_date);
+  let firstDate = new Date(item.first_air_date);
+  let genres = [];
+  for (let i in item.genres) {
+    genres.push(item.genres[i].name);
+  }
   return (
     <section
       className="featured"
@@ -11,7 +17,23 @@ export default ({ item }) => {
       }}
     >
       <div className="featured--vertical">
-        <div className="featured--horizontal"></div>
+        <div className="featured--horizontal">
+          <div className="featured--name">{item.original_name}</div>
+          <div className="featured--info">
+            <div className="featured--points">{item.vote_average} pontos</div>
+            <div className="featured--year">{firstDate.getFullYear()}</div>
+            <div className="featured--seasons">
+              {item.number_of_seasons} temporada
+              {item.number_of_seasons !== 1 ? "s" : ""}
+              <div className="featured--description">{item.overview}</div>
+              <div className="featured--buttons"></div>
+              <div className="featured--genres">
+                <strong>Gêneros:</strong>
+                {genres.join(", ")}
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </section>
   );
