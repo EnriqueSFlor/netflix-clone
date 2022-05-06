a61663b86a639e12808e5bd4525e3a4ff0288981
EnriqueSFlor <enriquesilvaflor@gmail.com>
2022-05-05 21:27:52 -0300

    incluindo imagens de original

--- src/components/FeaturedMovie.jsx
+++ src/components/FeaturedMovie.jsx
@@ -1,5 +1,18 @@
 import "./FeaturedMovie.css";
 
-export default () => {
-  return <div>FILME EM DESTAQUE</div>;
+export default ({ item }) => {
+  return (
+    <section
+      className="featured"
+      style={{
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        backgroundImage: `url(https://image.tmdb.org/t/p/original${item.backdrop_path})`,
+      }}
+    >
+      <div className="featured--vertical">
+        <div className="featured--horizontal"></div>
+      </div>
+    </section>
+  );
 };
