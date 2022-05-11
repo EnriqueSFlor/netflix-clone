c23bda44229f9452b3e9cdd290f26e4b505cffbb
EnriqueSFlor <enriquesilvaflor@gmail.com>
2022-05-10 23:03:27 -0300

    corrigindo dependencia de icons

--- src/components/MovieRow.js
+++ src/components/MovieRow.js
@@ -1,13 +1,18 @@
 import "./MovieRow.css";
 import React from "react";
 import NavigateBeforeIcon from "@mui/icons-material/NavigateBefore";
+import NavigateNextIcon from "@mui/icons-material/NavigateNext";
 
 export default ({ title, items }) => {
   return (
     <div className="movieRow">
       <h2>{title}</h2>
-      <div className="movieRow-left"></div>
-      <div className="movieRow--right"></div>
+      <div className="movieRow-left">
+        <NavigateBeforeIcon style={{ fontSize: 50 }} />
+      </div>
+      <div className="movieRow--right">
+        <NavigateNextIcon style={{ fontSize: 50 }} />
+      </div>
       <div className="movieRow--listarea">
         <div className="movieRow--list">
           {items.results.length > 0 &&
