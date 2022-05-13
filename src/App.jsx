b5ee548895b1d849446b721cff268cd8f11009c9
EnriqueSFlor <enriquesilvaflor@gmail.com>
2022-05-12 23:11:32 -0300

    finalizando

--- src/App.jsx
+++ src/App.jsx
@@ -61,6 +61,14 @@ export default () => {
         <br />
         Dados retirados do site Themoviedb.org
       </footer>
+      {movieList.length <= 0 && (
+        <div className="loading">
+          <img
+            src="https://media.filmelier.com/noticias/br/2020/03/Netflix_LoadTime.gif"
+            alt="Carregando"
+          />
+        </div>
+      )}
     </div>
   );
 };
